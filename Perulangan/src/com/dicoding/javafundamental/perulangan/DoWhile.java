104b3e86ad650bc935250dbc9cefb5ed30d48d52
Reski Mulud Muchamad <reski.mulud@gmail.com>
2021-05-07 19:47:44 +0700

    menambahkan project perulangan

---
+++ Perulangan/src/com/dicoding/javafundamental/perulangan/DoWhile.java
@@ -0,0 +1,13 @@
+package com.dicoding.javafundamental.perulangan;
+
+public class DoWhile {
+
+    public static void main(String[] args) {
+        int value = 1;
+        do {
+            System.out.println("Angka ke-" + value);
+            value++;
+        } while (value <= 10);
+    }
+
+}
